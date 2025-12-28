7c684cc6801850e75598e7d02141d8f6b7dd5ee7
User <gorbacheva.0021@bk.ru>
2025-12-29 02:41:41 +0300

    Hw № 8

--- Lesson_8/first.py
+++ Lesson_8/first.py
@@ -1,4 +1,51 @@
-"""Нужно создать папку с дз и сделать два файла с домашкой"""
+"""Реализовать программу для подсчёта индекса массы тела человека. Пользователь
+вводит рост и вес с клавиатуры. На выходе - ИМТ и пояснение к нему в зависимости
+от попадания в тот или иной диапазон. Использовать обработку исключений."""
 
-print("Hello world")
-print("Дописать изменения в файле")
+
+def num_input():
+    height = float(input("Введите ваш рост (м.): "))
+    weight = float(input("Введите ваш вес (кг.): "))
+    if height <= 0 or weight <= 0:
+        raise ValueError
+    return calc(height, weight)
+
+
+def calc(height, weight):
+    bmi = weight / height ** 2
+    if bmi < 16:
+        result = "выраженный дефицит массы тела"
+        return print_result(bmi, result)
+    elif 16 < bmi < 18.5:
+        result = "недостаточная (дефицит) масса тела"
+        return print_result(bmi, result)
+    elif 18.5 < bmi < 25:
+        result = "норма"
+        return print_result(bmi, result)
+    elif 25 < bmi < 30:
+        result = "избыточная масса тела (предожирение)"
+        return print_result(bmi, result)
+    elif 30 < bmi < 35:
+        result = "ожирение первой степени"
+        return print_result(bmi, result)
+    elif 35 < bmi < 40:
+        result = "ожирение второй степени"
+        return print_result(bmi, result)
+    elif bmi > 40:
+        result = "ожирение третьей степени (морбидное)"
+        return print_result(bmi, result)
+
+
+def print_result(bmi, result):
+    print(f"Ваш ИМТ: {bmi: .1f}. Это {result}.")
+
+
+def execute():
+    try:
+        num_input()
+    except ValueError:
+        print("Данные введены некорректно, попробуйте ещё раз")
+        return execute()
+
+
+execute()
--- Lesson_8/second.py
+++ Lesson_8/second.py
@@ -1,3 +1,49 @@
-"""Сделать второй файл и в нём написать принт"""
+"""Реализовать программу с функционалом калькулятора для операций над двумя числами.
+Числа и операция вводятся пользователем с клавиатуры. Использовать обработку исключений."""
 
-print("print")
+
+def num_input():
+    num_1, action, num_2 = input("Что посчитать? ").split()
+    num_1, num_2 = int(num_1), int(num_2)
+    return calc(num_1, action, num_2)
+
+
+def calc(num_1, action, num_2):
+    if action == "/":
+        result = num_1 / num_2
+        return print_result(num_1, action, num_2, result)
+    elif action == "*":
+        result = num_1 * num_2
+        return print_result(num_1, action, num_2, result)
+    elif action == "-":
+        result = num_1 - num_2
+        return print_result(num_1, action, num_2, result)
+    elif action == "+":
+        result = num_1 + num_2
+        return print_result(num_1, action, num_2, result)
+    elif action == "%":
+        result = num_1 % num_2
+        return print_result(num_1, action, num_2, result)
+    elif action == "//":
+        result = num_1 // num_2
+        return print_result(num_1, action, num_2, result)
+    else:
+        raise ValueError
+
+
+def print_result(num_1, action, num_2, result):
+    print(f"{num_1} {action} {num_2} = {result: .2f}")
+
+
+def execute():
+    try:
+        num_input()
+    except ZeroDivisionError:
+        print("Делитель не может быть нулём, попробуйте ещё раз")
+        return execute()
+    except ValueError:
+        print("Данные введены неверно, попробуйте ещё раз")
+        return execute()
+
+
+execute()
